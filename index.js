134e65ceaded637a8f2c0b4f182d56d5c2a9fc12
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-02-02 18:49:56 -0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = process.env.PORT || 3033;
+
+//middleware para recibir json
+app.use(express.json());
+
+//middleware para recibir form
+app.use(express.urlencoded((false)));
+
+app.use(router);
+
+
+app.listen(PORT, () => console.log(`serv running on http://localhost:${PORT}`));
